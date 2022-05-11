b50e57a3fdbf35bac50e92b9146f95ecc1648992
LeThanhTung0707 <tenanhlavip@gmail.com>
2022-05-11 14:44:06 +0700

    fix

--- backend-cms-nodejs/src/app/models/Admin.js
+++ backend-cms-nodejs/src/app/models/Admin.js
@@ -1,10 +1,11 @@
-const mongoose = require('mongoose'); 
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 const Admin = new Schema({
-  username: { type: String,},
-  password:{type:String},
-    category: { type: String,},
-    createdAt:{type:Date, default: Date.now},
-    updatedAt:{type:Date, default: Date.now},
-  });
-module.exports =mongoose.model('Admin', Admin); 
+  username: { type: String },
+  password: { type: String },
+  roles: { type: String, default: "user" },
+  category: { type: String },
+  createdAt: { type: Date, default: Date.now },
+  updatedAt: { type: Date, default: Date.now },
+});
+module.exports = mongoose.model("Admin", Admin);
