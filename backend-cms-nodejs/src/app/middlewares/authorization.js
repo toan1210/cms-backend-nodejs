c37941d06f73a507dfec692c8d1dda0fe14574bd
LeThanhTung0707 <tenanhlavip@gmail.com>
2022-05-11 14:32:51 +0700

    Thêm middleware phân quyền

--- backend-cms-nodejs/src/app/middlewares/authorization.js
+++ backend-cms-nodejs/src/app/middlewares/authorization.js
@@ -1,26 +1,32 @@
-const jwt = require('jsonwebtoken');
-
-module.exports.CheckLogin = function(req, res, next) {
-    
-    if(!req.cookies || !req.cookies.token) {
-        return res.json({
-            code: 401,
-            message: "Sai",
-            success: false
-        })
-        res.render('login/login/login')
-    //    return res.render('/login/login/login');
+const jwt = require("jsonwebtoken");
+const Admin = require("./../models/Admin");
+module.exports.CheckLogin = async function (req, res, next) {
+  try {
+    if (!req.cookies || !req.cookies.token) {
+      res.redirect("/");
+      //    return res.render('/login/login/login');
     }
-    var decoded = jwt.verify(req.cookies.token, 'mk');
-    if (!decoded._id) { 
-        return res.json({
-            code: 401,
-            message: "Đúng",
-            success: false
-        })
-        //return res.render('../../resources/views/home.hbs');
-        // return res.render('/login/login/login');
-        
+    var decoded = jwt.verify(req.cookies.token, "mk");
+    if (!decoded._id) {
+      return res.json({
+        code: 401,
+        message: "Đúng",
+        success: false,
+      });
+    }
+    const user = await Admin.findById(decoded._id);
+    req.user = user;
+    next();
+  } catch (error) {
+    next();
+  }
+};
+
+module.exports.restrictTo = (...roles) => {
+  return (req, res, next) => {
+    if (!roles.includes(req.user.roles)) {
+      return res.redirect("/home");
     }
     next();
-}
+  };
+};
--- backend-cms-nodejs/src/routes/admin.js
+++ backend-cms-nodejs/src/routes/admin.js
@@ -1,27 +1,33 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const path = require('path');
-const multer = require('multer');
+const path = require("path");
+const multer = require("multer");
+const auth = require("./../app/middlewares/authorization");
 const storage = multer.diskStorage({
-    destination:(req,file,cb) => {
-      cb(null,'src/public/images'); 
-    },
-    filename:(req,file,cb) => {
-      console.log(file);
-      cb(null,Date.now() + path.extname(file.originalname)); 
-    }
-  });
-  const upload = multer({storage:storage})
-const AdminControllers = require('../app/controllers/AdminControllers');
+  destination: (req, file, cb) => {
+    cb(null, "src/public/images");
+  },
+  filename: (req, file, cb) => {
+    console.log(file);
+    cb(null, Date.now() + path.extname(file.originalname));
+  },
+});
+const upload = multer({ storage: storage });
+const AdminControllers = require("../app/controllers/AdminControllers");
 //Phần Thêm Mới
-router.post("/add",upload.single('images'),AdminControllers.add);
+router.post("/add", upload.single("images"), AdminControllers.add);
 router.get("/create", AdminControllers.create);
-//Phần sửa 
-router.get('/:id/edit',AdminControllers.edit);
-router.put('/:id',AdminControllers.update);
+//Phần sửa
+router.get("/:id/edit", AdminControllers.edit);
+router.put("/:id", AdminControllers.update);
 //Phần Xóa
-router.delete('/:id',AdminControllers.delete);
+router.delete("/:id", AdminControllers.delete);
 
-router.get('/',AdminControllers.index);
+router.get(
+  "/",
+  auth.CheckLogin,
+  auth.restrictTo("admin"),
+  AdminControllers.index
+);
 
 module.exports = router;
